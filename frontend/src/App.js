5734af576a495d820d0336dc79d51fe52d8054fd
Pablo Satler <pablosatler@gmail.com>
2019-02-17 16:12:07 -0300

    :lipstick: style: updating the css of FileList component and importing it in the App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 
 import GlobalStyle from './styles/global';
-import { Container, Content } from './styles'
-import Upload from './components/Upload'
+import { Container, Content } from './styles';
+
+import Upload from './components/Upload';
+import FileList from './components/FileList';
 
 class App extends Component {
   render() {
@@ -10,6 +12,7 @@ class App extends Component {
       <Container>
         <Content>
           <Upload />
+          <FileList />
         </Content>
         <GlobalStyle />
       </Container>
--- frontend/src/components/FileList/index.js
+++ frontend/src/components/FileList/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import CircularProgressBar from 'react-circular-progressbar'
+import CircularProgressBar from 'react-circular-progressbar';
+import { MdCheckCircle, MdError, MdLink } from 'react-icons/md'
 
 import { Container, FileInfo, Preview } from './styles';
 
@@ -7,7 +8,7 @@ const FileList = () => (
     <Container>
         <li>
             <FileInfo>
-                <Preview src="http://localhost:3000/files/0c197d2016f07766b7da2aeefaf82532-FullSizeRender.jpg"/>
+                <Preview src="http://localhost:3001/files/0c197d2016f07766b7da2aeefaf82532-FullSizeRender.jpg"/>
                 <div>
                     <strong>profile.png</strong>
                     <span>64kb <button onClick={() => {}}> Delete! </button></span>
@@ -23,9 +24,18 @@ const FileList = () => (
                     }}
                     strokeWidth={10}
                     percentage={60}
-                >
+                />
 
-                </CircularProgressBar>
+                <a 
+                    href="http://localhost:3001/files/0c197d2016f07766b7da2aeefaf82532-FullSizeRender.jpg"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
+                </a>
+
+                <MdCheckCircle size={24} color="#78e5d5" />
+                <MdError size={24} color="#e57878" />
             </div>
         </li>
     </Container>
