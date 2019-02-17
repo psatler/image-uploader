d1a7c26871f4742402c062c3c91490a1daf1646b
Pablo Satler <pablosatler@gmail.com>
2019-02-17 17:03:36 -0300

    :sparkles: feat: add UI listing the files without uploading them yet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { uniqueId } from 'lodash';
+import filesize from 'filesize';
 
 import GlobalStyle from './styles/global';
 import { Container, Content } from './styles';
@@ -12,14 +14,32 @@ class App extends Component {
   }
 
   handleUpload = files => {
-    console.log(files)
-  }
+    // console.log(files)
+    const uploadedFiles = files.map(file => ({
+      file,
+      id: uniqueId(),
+      name: file.name,
+      readableSize: filesize(file.size),
+      preview: URL.createObjectURL(file), //here, generating a preview image url even before it getting to the backend server via the global URL
+      progress: 0,
+      uploaded: false, // if the uploaded has finished with success or not
+      error: false, // if there was an error with the upload
+      url: null, // url to be clicked for link of the image. It will only be filled once the upload has completed successfully
+    }));
+
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles),
+    });
+  };
 
   render() {
+    const { uploadedFiles } = this.state;
+
     return (
       <Container>
         <Content>
           <Upload onUpload={this.handleUpload} />
+          { !!uploadedFiles && <FileList files={uploadedFiles} />}
           <FileList />
         </Content>
         <GlobalStyle />
--- frontend/src/components/FileList/index.js
+++ frontend/src/components/FileList/index.js
@@ -4,40 +4,52 @@ import { MdCheckCircle, MdError, MdLink } from 'react-icons/md'
 
 import { Container, FileInfo, Preview } from './styles';
 
-const FileList = () => (
+const FileList = ({ files }) => (
     <Container>
-        <li>
-            <FileInfo>
-                <Preview src="http://localhost:3001/files/0c197d2016f07766b7da2aeefaf82532-FullSizeRender.jpg"/>
+        { files && files.map(uploadedFile => (
+            <li>
+                <FileInfo>
+                    <Preview src={uploadedFile.preview}/>
+                    <div>
+                        <strong>{uploadedFile.name}</strong>
+                        <span>
+                            {uploadedFile.readableSize} 
+                            {!!uploadedFile.url && <button onClick={() => {}}> Delete! </button> }
+                        </span>
+                    </div>
+                </FileInfo>
+
                 <div>
-                    <strong>profile.png</strong>
-                    <span>64kb <button onClick={() => {}}> Delete! </button></span>
+                    {/* if the file has not finish uploading yet and has't have an error, show progress bar */}
+                    {!uploadedFile.uploaded && !uploadedFile.error && (
+                        <CircularProgressBar 
+                            styles={{
+                                // root is div that wraps the progress bar (the outmost one)
+                                root: { width: 24 }, // the size wanted for the bar
+                                path: { stroke: '#7159c1'}, // the color of the progress path itself
+                            }}
+                            strokeWidth={10}
+                            percentage={uploadedFile.progress}
+                        />
+                    )}
+                    
+                    {/* if it has a link, it can be showed */}
+                    {uploadedFile.url && (
+                        <a 
+                            href="http://localhost:3001/files/0c197d2016f07766b7da2aeefaf82532-FullSizeRender.jpg"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
+                        </a>
+                    )}
+                    
+                    {uploadedFile.uploaded && <MdCheckCircle size={24} color="#78e5d5" />}
+                    {uploadedFile.error && <MdError size={24} color="#e57878" />}
+                    
                 </div>
-            </FileInfo>
-
-            <div>
-                <CircularProgressBar 
-                    styles={{
-                        // root is div that wraps the progress bar (the outmost one)
-                        root: { width: 24 }, // the size wanted for the bar
-                        path: { stroke: '#7159c1'}, // the color of the progress path itself
-                    }}
-                    strokeWidth={10}
-                    percentage={60}
-                />
-
-                <a 
-                    href="http://localhost:3001/files/0c197d2016f07766b7da2aeefaf82532-FullSizeRender.jpg"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
-                </a>
-
-                <MdCheckCircle size={24} color="#78e5d5" />
-                <MdError size={24} color="#e57878" />
-            </div>
-        </li>
+            </li>
+        ))}
     </Container>
 )
     
